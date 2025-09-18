df3a1da8840d2d0ef8fc72bf0acfa1f6e483f169
CiroMirkin <ciromirkin@gmail.com>
2025-09-18 10:06:04 -0300

    agrega: archivo serviceCodes

--- src/domain/classifyServices.ts
+++ src/domain/classifyServices.ts
@@ -1,4 +1,5 @@
 import type { Servicio } from "@/model/service"
+import { accidentsCodes, firesCodes } from "@/model/serviceCodes"
 
 export interface ClassifiedServices {
   accidentes: Servicio[]
@@ -13,19 +14,9 @@ export interface ClassifiedServices {
 export function classifyServices(
   servicios: Servicio[]
 ): ClassifiedServices {
-  const tiposIncendio = [
-    "1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H", "1J", "1I",
-  ]
+  const tiposIncendio = firesCodes
 
-  const tiposAccidente = ["2A",
-    "2B",
-    "2C",
-    "2D",
-    "2E",
-    "2F",
-    "2G",
-    "2H",
-  ]
+  const tiposAccidente = accidentsCodes
 
   const incendiosSet = new Set(tiposIncendio)
   const accidentesSet = new Set(tiposAccidente)
---
+++ src/model/serviceCodes.ts
@@ -0,0 +1,8 @@
+
+export const firesCodes = Object.freeze([
+    "1A", "1B", "1C", "1D", "1E", "1F", "1G", "1H", "1J", "1I",
+])
+
+export const accidentsCodes =  Object.freeze([
+    "2A","2B", "2C", "2D","2E", "2F", "2G", "2H",
+])
